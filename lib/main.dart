3780114d1bca542547f17fbd042b3a66b0f512cc
Khanin <khanin.dev@gmail.com>
2022-11-15 16:51:10 +0700

    simplify code logic for new simplify ux

--- lib/main.dart
+++ lib/main.dart
@@ -33,18 +33,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Map<String, String> convertingUnit = {'sqm': 'ไร่'};
-
   double _sqm = 1600;
   double _rai = 1;
   double _ngan = 0;
-  double _sqWaa = 0;
-  double _totalSqWaa = 0;
+  double _sqWha = 0;
+  double _fullRai = 0;
+  double _fullNgan = 0;
+  double _fullSqWha = 0;
+  double _totalSqWha = 0;
 
-  final _sqmTextController = TextEditingController(text: '1600');
-  final _raiTextController = TextEditingController(text: '1');
-  final _nganTextController = TextEditingController(text: '0');
-  final _sqWaaTextController = TextEditingController(text: '0');
+  ConvertingUnit selectedUnit = ConvertingUnit.sqm;
+
+  // final _sqmTextController = TextEditingController(text: '1600');
+  // final _raiTextController = TextEditingController(text: '1');
+  // final _nganTextController = TextEditingController(text: '0');
+  // final _sqWaaTextController = TextEditingController(text: '0');
+  final _inputTextController = TextEditingController(text: '1600');
 
   String getUnitText(ConvertingUnit unit) {
     switch (unit) {
@@ -67,42 +71,52 @@ class _MyHomePageState extends State<MyHomePage> {
     _sqm = (n != null) ? n : 0;
     double sqWaaRemainder;
     setState(() {
-      _totalSqWaa = _sqm / 4;
-      _rai = (_totalSqWaa / 400).floorToDouble();
-      sqWaaRemainder = _totalSqWaa.remainder(400);
+      _totalSqWha = _sqm / 4;
+      _rai = (_totalSqWha / 400).floorToDouble();
+      sqWaaRemainder = _totalSqWha.remainder(400);
       _ngan = (sqWaaRemainder / 100).floorToDouble();
-      _sqWaa = sqWaaRemainder.remainder(100);
+      _sqWha = sqWaaRemainder.remainder(100);
 
-      _raiTextController.text = _rai.toString();
-      _nganTextController.text = _ngan.toString();
-      _sqWaaTextController.text = _sqWaa.toString();
+      _fullRai = _sqm / 400;
+      _fullNgan = _sqm / 100;
+      _fullSqWha = _sqm / 4;
+
+      // _raiTextController.text = _rai.toString();
+      // _nganTextController.text = _ngan.toString();
+      // _sqWaaTextController.text = _sqWaa.toString();
     });
   }
 
-  void updateSqmState() {
+  void selectUnit(newUnit) {
     setState(() {
-      _sqm = (_rai * 1600) + (_ngan * 400) + (_sqWaa * 4);
-      _sqmTextController.text = _sqm.toString();
+      selectedUnit = newUnit;
     });
   }
 
-  void convertRai(String txtRaiInput) {
-    var n = double.tryParse(txtRaiInput);
-    _rai = (n != null) ? n : 0;
-    updateSqmState();
-  }
+  // void updateSqmState() {
+  //   setState(() {
+  //     _sqm = (_rai * 1600) + (_ngan * 400) + (_sqWaa * 4);
+  //     _sqmTextController.text = _sqm.toString();
+  //   });
+  // }
 
-  void convertNgan(String txtRaiInput) {
-    var n = double.tryParse(txtRaiInput);
-    _ngan = (n != null) ? n : 0;
-    updateSqmState();
-  }
+  // void convertRai(String txtRaiInput) {
+  //   var n = double.tryParse(txtRaiInput);
+  //   _rai = (n != null) ? n : 0;
+  //   updateSqmState();
+  // }
 
-  void convertSqWaa(String txtRaiInput) {
-    var n = double.tryParse(txtRaiInput);
-    _sqWaa = (n != null) ? n : 0;
-    updateSqmState();
-  }
+  // void convertNgan(String txtRaiInput) {
+  //   var n = double.tryParse(txtRaiInput);
+  //   _ngan = (n != null) ? n : 0;
+  //   updateSqmState();
+  // }
+
+  // void convertSqWaa(String txtRaiInput) {
+  //   var n = double.tryParse(txtRaiInput);
+  //   _sqWaa = (n != null) ? n : 0;
+  //   updateSqmState();
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -115,14 +129,64 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           children: [
             Text('เปลี่ยนหน่วยที่ดินตามรางเมตร'),
-            InputRow(
-              unit: _sqm,
-              inputLabel: 'ตรม.',
-              unitLabel: 'ตรม.',
-              textEditingController: _sqmTextController,
-              onChanged: convertSqm,
-              getUnitText: getUnitText,
+            Row(
+              children: [
+                Flexible(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                    child: TextField(
+                      onChanged: convertSqm,
+                      controller: _inputTextController,
+                      keyboardType:
+                          TextInputType.numberWithOptions(decimal: true),
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.allow(
+                          RegExp(r'[0-9]'),
+                        ),
+                      ],
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                ),
+                Flexible(
+                  flex: 2,
+                  child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                      // child: Text(unitLabel)),
+                      child: DropdownButton<ConvertingUnit>(
+                        value: selectedUnit,
+                        items: ConvertingUnit.values.map((ConvertingUnit unit) {
+                          return DropdownMenuItem<ConvertingUnit>(
+                              value: unit, child: Text(getUnitText(unit)));
+                        }).toList(),
+                        onChanged: selectUnit,
+                      )),
+                )
+              ],
             ),
+            Row(
+              children: [Text('= $_rai ไร่ $_ngan งาน $_sqWha ตรว.')],
+            ),
+            Row(
+              children: [Text('= $_fullRai ไร่')],
+            ),
+            Row(
+              children: [Text('= $_fullNgan งาน')],
+            ),
+            Row(
+              children: [Text('= $_fullSqWha ตรว.')],
+            ),
+            // InputRow(
+            //   unit: _sqm,
+            //   inputLabel: 'ตรม.',
+            //   unitLabel: 'ตรม.',
+            //   textEditingController: _sqmTextController,
+            //   onChanged: convertSqm,
+            //   getUnitText: getUnitText,
+            //   selectedUnit: ConvertingUnit.sqm,
+            // ),
             // Text('เปลี่ยนหน่วยที่ดินไร่'),
             // InputRow(
             //     unit: _rai,
@@ -150,60 +214,62 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class InputRow extends StatelessWidget {
-  InputRow(
-      {Key? key,
-      required this.unit,
-      required this.inputLabel,
-      required this.unitLabel,
-      required this.onChanged,
-      required this.getUnitText,
-      required this.textEditingController})
-      : super(key: key);
+// class InputRow extends StatelessWidget {
+//   InputRow({
+//     Key? key,
+//     required this.unit,
+//     required this.inputLabel,
+//     required this.unitLabel,
+//     required this.onChanged,
+//     required this.getUnitText,
+//     required this.textEditingController,
+//     required this.selectedUnit,
+//   }) : super(key: key);
 
-  double unit;
-  final String inputLabel;
-  final String unitLabel;
-  final Function(String) onChanged;
-  final Function(ConvertingUnit) getUnitText;
-  final TextEditingController textEditingController;
+//   double unit;
+//   final String inputLabel;
+//   final String unitLabel;
+//   final Function(String) onChanged;
+//   final Function(ConvertingUnit) getUnitText;
+//   final TextEditingController textEditingController;
+//   ConvertingUnit selectedUnit;
 
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Flexible(
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-            child: TextField(
-              onChanged: onChanged,
-              controller: textEditingController,
-              keyboardType: TextInputType.numberWithOptions(decimal: true),
-              inputFormatters: <TextInputFormatter>[
-                FilteringTextInputFormatter.allow(
-                  RegExp(r'[0-9]'),
-                ),
-              ],
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), labelText: inputLabel),
-            ),
-          ),
-        ),
-        Flexible(
-          flex: 2,
-          child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-              // child: Text(unitLabel)),
-              child: DropdownButton<ConvertingUnit>(
-                value: ConvertingUnit.sqm,
-                items: ConvertingUnit.values.map((ConvertingUnit unit) {
-                  return DropdownMenuItem<ConvertingUnit>(
-                      value: unit, child: Text(getUnitText(unit)));
-                }).toList(),
-                onChanged: (value) {},
-              )),
-        )
-      ],
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       children: [
+//         Flexible(
+//           child: Container(
+//             padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+//             child: TextField(
+//               onChanged: onChanged,
+//               controller: textEditingController,
+//               keyboardType: TextInputType.numberWithOptions(decimal: true),
+//               inputFormatters: <TextInputFormatter>[
+//                 FilteringTextInputFormatter.allow(
+//                   RegExp(r'[0-9]'),
+//                 ),
+//               ],
+//               decoration: InputDecoration(
+//                   border: OutlineInputBorder(), labelText: inputLabel),
+//             ),
+//           ),
+//         ),
+//         Flexible(
+//           flex: 2,
+//           child: Container(
+//               padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+//               // child: Text(unitLabel)),
+//               child: DropdownButton<ConvertingUnit>(
+//                 value: selectedUnit,
+//                 items: ConvertingUnit.values.map((ConvertingUnit unit) {
+//                   return DropdownMenuItem<ConvertingUnit>(
+//                       value: unit, child: Text(getUnitText(unit)));
+//                 }).toList(),
+//                 onChanged: (value) {},
+//               )),
+//         )
+//       ],
+//     );
+//   }
+// }
