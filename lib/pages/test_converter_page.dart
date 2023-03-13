3a21d6b2d050a33a1941ad7f5532921f78468ee7
Khanin <khanin.dev@gmail.com>
2023-03-13 22:27:34 +0700

    ui condition based on selected unit

--- lib/pages/test_converter_page.dart
+++ lib/pages/test_converter_page.dart
@@ -158,26 +158,36 @@ class TestConverterPageState extends ConsumerState<TestConverterPage> {
                 ),
                 child: Column(
                   children: [
-                    ResultRow(
-                      resultText:
-                          '${getInputText(singleInputCtrl, calState)} = ${kNumFormat.format(calState.rai)} ไร่ ${kNumFormat.format(calState.ngan)} งาน ${kNumFormat.format(calState.sqWha)} ตรว.',
-                    ),
-                    ResultRow(
-                      resultText:
-                          '${getInputText(singleInputCtrl, calState)} = ${kNumFormat.format(calState.sqm)} ตรม.',
-                    ),
-                    ResultRow(
-                      resultText:
-                          '${getInputText(singleInputCtrl, calState)} = ${kNumFormat.format(calState.fullRai)} ไร่.',
-                    ),
-                    ResultRow(
-                      resultText:
-                          '${getInputText(singleInputCtrl, calState)} = ${kNumFormat.format(calState.fullNgan)} งาน.',
-                    ),
-                    ResultRow(
-                      resultText:
-                          '${getInputText(singleInputCtrl, calState)} = ${kNumFormat.format(calState.fullSqWha)} ตรว.',
-                    ),
+                    calState.selectedUnit != ConvertingUnit.raiNganSqWha
+                        ? ResultRow(
+                            resultText:
+                                '${getInputText(singleInputCtrl, calState)} = ${kNumFormat.format(calState.rai)} ไร่ ${kNumFormat.format(calState.ngan)} งาน ${kNumFormat.format(calState.sqWha)} ตรว.',
+                          )
+                        : const SizedBox(),
+                    calState.selectedUnit != ConvertingUnit.sqm
+                        ? ResultRow(
+                            resultText:
+                                '${getInputText(singleInputCtrl, calState)} = ${kNumFormat.format(calState.sqm)} ตรม.',
+                          )
+                        : const SizedBox(),
+                    calState.selectedUnit != ConvertingUnit.rai
+                        ? ResultRow(
+                            resultText:
+                                '${getInputText(singleInputCtrl, calState)} = ${kNumFormat.format(calState.fullRai)} ไร่.',
+                          )
+                        : const SizedBox(),
+                    calState.selectedUnit != ConvertingUnit.ngan
+                        ? ResultRow(
+                            resultText:
+                                '${getInputText(singleInputCtrl, calState)} = ${kNumFormat.format(calState.fullNgan)} งาน.',
+                          )
+                        : const SizedBox(),
+                    calState.selectedUnit != ConvertingUnit.sqWha
+                        ? ResultRow(
+                            resultText:
+                                '${getInputText(singleInputCtrl, calState)} = ${kNumFormat.format(calState.fullSqWha)} ตรว.',
+                          )
+                        : const SizedBox(),
                   ],
                 ),
               ),
