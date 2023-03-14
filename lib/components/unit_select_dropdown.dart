ccbb282a0048515cabbc9143050381d1d8ffb8d1
Khanin <khanin.dev@gmail.com>
2023-03-14 22:58:38 +0700

    refactor

---
+++ lib/components/unit_select_dropdown.dart
@@ -0,0 +1,56 @@
+import 'package:convert_rai/calculate_logic.dart';
+import 'package:convert_rai/helper_function.dart';
+import 'package:flutter/material.dart';
+
+class UnitSelectDropdown extends StatelessWidget {
+  const UnitSelectDropdown(
+      {super.key,
+      required this.calState,
+      required this.calNotifier,
+      required this.singleInputCtrl,
+      required this.raiInputCtrl,
+      required this.nganInputCtrl,
+      required this.sqWhaInputCtrl});
+
+  final Calculation calState;
+  final CalNotifier calNotifier;
+  final TextEditingController singleInputCtrl;
+  final TextEditingController raiInputCtrl;
+  final TextEditingController nganInputCtrl;
+  final TextEditingController sqWhaInputCtrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(12),
+        ),
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+      child: DropdownButton<ConvertingUnit>(
+        borderRadius: const BorderRadius.all(Radius.circular(12)),
+        alignment: Alignment.center,
+        value: calState.selectedUnit,
+        items: ConvertingUnit.values.map((ConvertingUnit unit) {
+          return DropdownMenuItem<ConvertingUnit>(
+              value: unit, child: Text(getUnitText(unit)));
+        }).toList(),
+        onChanged: (newUnit) {
+          calNotifier.selectUnit(newUnit);
+
+          if (newUnit != ConvertingUnit.raiNganSqWha) {
+            double n = stringToDouble(singleInputCtrl.text);
+            calNotifier.convertUnit(n);
+          } else {
+            double rai = stringToDouble(raiInputCtrl.text);
+            double ngan = stringToDouble(nganInputCtrl.text);
+            double sqWha = stringToDouble(sqWhaInputCtrl.text);
+            calNotifier.convertCombinedUnit(rai, ngan, sqWha);
+          }
+        },
+      ),
+    );
+  }
+}
--- lib/pages/test_converter_page.dart
+++ lib/pages/test_converter_page.dart
@@ -1,5 +1,7 @@
 import 'package:convert_rai/calculate_logic.dart';
+import 'package:convert_rai/components/custom_input.dart';
 import 'package:convert_rai/components/result_row.dart';
+import 'package:convert_rai/components/select_dropdown.dart';
 import 'package:convert_rai/constants.dart';
 import 'package:convert_rai/helper_function.dart';
 import 'package:flutter/material.dart';
@@ -203,96 +205,6 @@ class TestConverterPageState extends ConsumerState<TestConverterPage> {
     raiInputCtrl.text = '';
     nganInputCtrl.text = '';
     sqWhaInputCtrl.text = '';
-  }
-}
-
-class CustomInputField extends StatelessWidget {
-  const CustomInputField(
-      {super.key,
-      required this.inputTextController,
-      required this.calNotifier,
-      required this.onChanged,
-      required this.label});
-
-  final TextEditingController inputTextController;
-  final CalNotifier calNotifier;
-  final Function(String) onChanged;
-  final String label;
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      controller: inputTextController,
-      onChanged: onChanged,
-      keyboardType: const TextInputType.numberWithOptions(decimal: true),
-      inputFormatters: [ThousandsFormatter()],
-      decoration: InputDecoration(
-        // label: Text(label),
-        hintText: label,
-        fillColor: Colors.white,
-        filled: true,
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12.0),
-          borderSide: const BorderSide(
-            width: 0,
-            style: BorderStyle.none,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class UnitSelectDropdown extends StatelessWidget {
-  const UnitSelectDropdown(
-      {super.key,
-      required this.calState,
-      required this.calNotifier,
-      required this.singleInputCtrl,
-      required this.raiInputCtrl,
-      required this.nganInputCtrl,
-      required this.sqWhaInputCtrl});
-
-  final Calculation calState;
-  final CalNotifier calNotifier;
-  final TextEditingController singleInputCtrl;
-  final TextEditingController raiInputCtrl;
-  final TextEditingController nganInputCtrl;
-  final TextEditingController sqWhaInputCtrl;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.all(
-          Radius.circular(12),
-        ),
-      ),
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-      child: DropdownButton<ConvertingUnit>(
-        borderRadius: const BorderRadius.all(Radius.circular(12)),
-        alignment: Alignment.center,
-        value: calState.selectedUnit,
-        items: ConvertingUnit.values.map((ConvertingUnit unit) {
-          return DropdownMenuItem<ConvertingUnit>(
-              value: unit, child: Text(getUnitText(unit)));
-        }).toList(),
-        onChanged: (newUnit) {
-          calNotifier.selectUnit(newUnit);
-
-          if (newUnit != ConvertingUnit.raiNganSqWha) {
-            double n = stringToDouble(singleInputCtrl.text);
-            calNotifier.convertUnit(n);
-          } else {
-            double rai = stringToDouble(raiInputCtrl.text);
-            double ngan = stringToDouble(nganInputCtrl.text);
-            double sqWha = stringToDouble(sqWhaInputCtrl.text);
-            calNotifier.convertCombinedUnit(rai, ngan, sqWha);
-          }
-        },
-      ),
-    );
   }
 }
 
