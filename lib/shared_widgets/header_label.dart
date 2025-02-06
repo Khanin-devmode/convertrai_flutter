087db6988b16de86ddccc9d393d004b642febc2e
Khanin <khanin.dev@gmail.com>
2025-02-06 15:13:13 +0700

    add ui logo and enhance ui

--- lib/shared_widgets/header_label.dart
+++ lib/shared_widgets/header_label.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:convert_rai/features/main_page/presentation/main_app_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -9,45 +10,77 @@ class HeaderRow extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final currentLang = ref.watch(localLangCodeProvider);
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 18),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            label,
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 26,
-              fontWeight: FontWeight.w600,
+      child: SizedBox(
+        height: 40,
+        child: Row(
+          children: [
+            SizedBox(width: 8),
+            Image.asset(
+              'assets/images/rai_logo.png',
+              height: 28,
             ),
-          ),
-          Row(
-            children: [
-              TextButton(
-                  onPressed: (() => ref
-                      .read(localLangCodeProvider.notifier)
-                      .update((state) => 'th')),
-                  child: const Text(
-                    'ไทย',
-                    style: TextStyle(color: Colors.white),
-                  )),
-              Container(
-                width: 2,
-                height: 24,
-                color: Colors.white,
+            SizedBox(width: 8),
+            Expanded(
+              child: AutoSizeText(
+                label,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 26,
+                  fontWeight: FontWeight.w600,
+                ),
+                minFontSize: 16,
+                maxLines: 1,
               ),
-              TextButton(
-                  onPressed: (() => ref
-                      .read(localLangCodeProvider.notifier)
-                      .update((state) => 'en')),
-                  child: const Text(
-                    'En',
-                    style: TextStyle(color: Colors.white),
-                  )),
-            ],
-          ),
-        ],
+            ),
+            Row(
+              children: [
+                GestureDetector(
+                    onTap: (() => ref
+                        .read(localLangCodeProvider.notifier)
+                        .update((state) => 'th')),
+                    child: Container(
+                      alignment: Alignment.center,
+                      width: 48,
+                      child: Text(
+                        'ไทย',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: currentLang == 'th'
+                              ? FontWeight.bold
+                              : FontWeight.normal,
+                        ),
+                      ),
+                    )),
+                Container(
+                  width: 2,
+                  height: 24,
+                  color: Colors.white,
+                ),
+                GestureDetector(
+                    onTap: (() => ref
+                        .read(localLangCodeProvider.notifier)
+                        .update((state) => 'en')),
+                    child: Container(
+                      width: 48,
+                      alignment: Alignment.center,
+                      child: Text(
+                        'En',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: currentLang == 'en'
+                              ? FontWeight.bold
+                              : FontWeight.normal,
+                        ),
+                      ),
+                    )),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
